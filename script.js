5400a6d89e30f5806215b1df34b4d924e3f35034
Abdullah-Ed <hssinouabdullah@gmail.com>
2023-03-28 19:00:09 +0200

    Add a Function that declares the winner

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+function gameRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else if(playerSelection === 'paper' && computerSelection === 'rock'){
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  }else{
+    console.log('Tie')
+  }
+}
+
+let playerSelection = 'rock';
+let computerSelection = getComputerChoice();
+
+console.log(`Computer chose: ${computerSelection}`);
+gameRound(playerSelection, computerSelection);
